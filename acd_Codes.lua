c45b3cf0a28d971717a9504025b3599074ffa5b9
buang <139893805+buang5516@users.noreply.github.com>
2024-05-14 03:41:13 +0800

    Update acd_Codes.lua

--- acd_Codes.lua
+++ acd_Codes.lua
@@ -1,6 +1,7 @@
 local codes = {
   'Valk', 'Release', 'Sebbyastian', 'MozKing', 'KingLuffy',
-  'Fish', '10k', '15k', '1mvisits', 'StarCodeVanilla', 'blamspot'
+  'Fish', '10k', '15k', '1mvisits', 'StarCodeVanilla', 'blamspot',
+  'Zerozkingner', 'BlamSpot'
 }
 
 return codes
