caf180665999cd3c3e51123191b5e88455a5d40c
buang <139893805+buang5516@users.noreply.github.com>
2024-05-17 00:35:04 +0800

    Update BUANGHUB.lua

--- BUANGHUB.lua
+++ BUANGHUB.lua
@@ -1,7 +1,8 @@
 local games = {
 	[12229756] = 'https://api.luarmor.net/files/v3/loaders/2693a688362ddb52f452a288ea86ba89.lua', -- ALS
 	[5292947] =  'https://api.luarmor.net/files/v3/loaders/5626ab81ffceae865d22d54cb5042edf.lua', -- ASTD
-	[15022320] = 'https://api.luarmor.net/files/v3/loaders/5626ab81ffceae865d22d54cb5042edf.lua' -- TTD
+	[15022320] = 'https://api.luarmor.net/files/v3/loaders/5626ab81ffceae865d22d54cb5042edf.lua', -- TTD
+	[5102326] = 'https://api.luarmor.net/files/v3/loaders/1b4c42f5913d7a5b7be56ee7766eb814.lua' -- ACD
 }
 
 if games[game.CreatorId] then
