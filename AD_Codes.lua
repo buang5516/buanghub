8ad32a1d87b665ca6278022b7df88f007d346e0c
buang <139893805+buang5516@users.noreply.github.com>
2024-05-27 01:10:13 +0800

    Create AD_Codes.lua

---
+++ AD_Codes.lua
@@ -0,0 +1,12 @@
+local codes = {
+  'subcool',
+  'sub2toadboigaming',
+  'sub2mozking',
+  'sub2karizmaqt',
+  'sub2jonaslyz',
+  'sub2riktime',
+  'sub2nagblox',
+  'release2024'
+}
+
+return codes;
