3e8f1cdebe4af4a86a248dadddeab07cfdd2c56d
buang <139893805+buang5516@users.noreply.github.com>
2024-05-27 03:26:29 +0800

    Update PremiumBuangHub.lua

--- PremiumBuangHub.lua
+++ PremiumBuangHub.lua
@@ -3,7 +3,8 @@ local games = {
 	[5292947] =  'https://api.luarmor.net/files/v3/loaders/eef1f2c7ccd0aaf83d95f052ef2e934a.lua',
 	[32763927] = 'https://api.luarmor.net/files/v3/loaders/a53e36936ee27fdfb837b72aa51e249c.lua',
 	[15022320] = 'https://api.luarmor.net/files/v3/loaders/07d71dd1339d9d813e40e938d824bdcc.lua',
-	[5102326] = 'https://api.luarmor.net/files/v3/loaders/ce64962ee176c33b012b9981c4b54b1a.lua' -- ACD
+	[5102326] = 'https://api.luarmor.net/files/v3/loaders/ce64962ee176c33b012b9981c4b54b1a.lua', -- ACD
+	[34121350] = 'https://api.luarmor.net/files/v3/loaders/b648cec51b1d83aab602bbee4e2a8c6f.lua'
 }
 
 if games[game.CreatorId] then
