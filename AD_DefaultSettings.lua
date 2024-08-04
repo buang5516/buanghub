6029880b1816e909de7695091f736ef7723b3a75
buang <139893805+buang5516@users.noreply.github.com>
2024-08-04 16:48:25 +0800

    Create AD_DefaultSettings.lua

---
+++ AD_DefaultSettings.lua
@@ -0,0 +1,125 @@
+local dSettings = {
+	['FirstTime'] = true,
+	['modeSelect'] = 'Story',
+	['mapSelect'] = 'FooshaVillage',
+	['actSelect'] = '1',
+	['difficultySelect'] = 'Normal',
+	['friendSelect'] = false,
+	['AutoJoinMap'] = false,
+	['AutoStart'] = false,
+	['Auto Start Delay'] = 4,
+	['Auto Start Time Delay'] = 30,
+	['Auto Leave'] = false,
+	['Auto Next'] = false,
+	['Auto Replay'] = false,
+	['Webhook'] = {
+		['URL'] = '',
+		['ID'] = 0,
+		['Ping'] = false,
+		['Result'] = false
+	},
+	['WebhookPingSelected'] = {},
+	['PingSelected'] = false,
+
+	['Macro Mode Select'] = 'Story',
+	['Macros Selected'] = {
+		['Story'] = {},
+		['Infinite'] = {},
+		['Challenge'] = {},
+		['Raid'] = {},
+		['Tower'] = {
+			['Map1'] = '', ['Map2'] = '', ['Map3'] = '', ['Map4'] = '', ['Map5'] = ''
+		},
+		['HallOfMirrors'] = {
+			['Easy'] = '', ['Normal'] = '', ['Hard'] = '', ['Nightmare'] = ''
+		},
+		['Portal'] = {}
+	},
+	['Record Options'] = 'Money',
+	['Auto Summon'] = false,
+	['blackScreen'] = false,
+	['Step Delay'] = 3,
+	['Selected Macro'] = '',
+	['TP Delay'] = 3,
+	['TP Lobby Time'] = 10,
+	['TP Lobby'] = false,
+	['Hide Key'] = 'U',
+
+	['Export File'] = '',
+	['Import Mode'] = 'Link',
+	['Summon Mode'] = 'All Gems',
+	['Summon Banner'] = 'Standard',
+	['Summon Unit'] = '',
+	['Summon Select'] = '1',
+	['Summon Gems'] = 0,
+
+	['Auto Sell Wave'] = 1,
+	['AutoSellFarmsWave'] = 1,
+	['AutoLeaveOnWave'] = 1,
+	['Auto Leave Wave'] = false,
+
+	['Auto Joiner Player Select'] = '',
+	['Auto Joiner Player'] = false,
+
+	['IgnoreChallengeMap'] = {},
+	['IgnoreChallenge'] = {},
+	['IgnoreChallengeRewards'] = {},
+	['AutoChallenge'] = false,
+
+	['Auto Skip Wave'] = false,
+	['Auto Raid'] = false,
+	['Auto TP Raid Open'] = false,
+	['Selected Raids'] = {},
+
+	['Raid Buy List'] = {},
+	['Auto Buy Raid'] = false,
+	['Raid2 Buy List'] = {},
+	['Auto Buy Raid2'] = false,
+	['Auto Story Mode'] = 'Automatic',
+	['Selected Story'] = 'FooshaVillage_Chapter1',
+	['Auto Story'] = false,
+
+	['Auto Tower'] = false,
+	['Auto Delete Rarity'] = {},
+	['Enable Auto Delete Rarity'] = false;
+	['Auto Sell Rarity'] = {},
+	['Enable Auto Sell Rarity'] = false,
+	['Ignore Shiny Auto Sell Rarity'] = true,
+	['Join Priority'] = {
+		['1'] = 'Auto Join Player',
+		['2'] = 'Auto Tower',
+		['3'] = 'Auto Raid',
+		['4'] = 'Auto Challenge',
+		['5'] = 'Auto Story',
+		['6'] = 'Auto Join Map',
+		['7'] = 'Auto Portal'
+	},
+	['Auto Leave when No Enemy Spawn'] = false,
+
+	['Auto Trait Reroll'] = false,
+	['Selected Traits Reroll'] = {},
+
+	['Auto Portal Mode'] = {},
+	['Selected Portal Tiers'] = {},
+	['Auto Portal'] = false,
+	['CloudSave'] = {},
+	['ConvertTickets'] = 0,
+	['Auto Convert Tickets'] = false,
+	['Auto Claim Battlepass'] = false,
+	['SelectUnitsToTrade'] = {},
+	['SelectItemsToTrade'] = {},
+	['TradePlayer'] = '',
+	['Auto Trade'] = false,
+	['Trade Main Account'] = false,
+	['Send Gems Players'] = {},
+	['Auto Send Gems'] = false,
+	['asd']  = '',
+	['dsa'] = 0,
+	['dsa2'] = 0,
+	['dsa3'] = 0,
+	['Booth Snipe'] = {},
+	['Safety Players'] = {},
+	['Enable Safety Players'] = false
+}
+
+return dSettings;
