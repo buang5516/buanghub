8535e6efc183624b6522a3ad8b9a533b6dbeacb8
buang <139893805+buang5516@users.noreply.github.com>
2024-09-05 06:57:01 +0800

    Update codes.lua

---
+++ SAD/codes.lua
@@ -0,0 +1,28 @@
+local codes = {
+  "Restock!",
+  "HW12",
+  "10mil",
+  "30klikes",
+  "100k!",
+  "90K!",
+  "Future!",
+  "20K!",
+  "Invaded!",
+  "DataFix!",
+  "Sorry!",
+  "Delay",
+  "1MVISITS",
+  "50KDiscord!",
+  "8klikes",
+  "10kfavs",
+  "12K",
+  "RELEASE",
+  "Toadboigaming",
+  "ServerAttack!",
+  "Gems",
+  "Trait",
+  "YOU",
+  "GizwopOnTop"
+}
+
+return codes;
