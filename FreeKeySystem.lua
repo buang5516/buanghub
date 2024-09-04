052a1aaa2401eb553164ab5a5632e94211dfdea8
buang <139893805+buang5516@users.noreply.github.com>
2024-09-04 08:16:42 +0800

    Create FreeKeySystem.lua

---
+++ FreeKeySystem.lua
@@ -0,0 +1,17 @@
+if not game:IsLoaded() then
+  game.Loaded:Wait()
+end
+
+task.wait( math.random() )
+
+local games = {
+    [33859442] = 'https://api.luarmor.net/files/v3/loaders/e211d81f5139290b3ca47d3679b3f176.lua' -- Anime Royale
+}
+
+if games[game.CreatorId] ~= nil and script_key ~= nil then
+  task.wait( math.random() )
+  repeat
+    loadstring(game:HttpGet( games[game.CreatorId] ))()
+    task.wait(10)
+  until game.buanghub ~= nil;
+end
