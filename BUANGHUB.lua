c1aec3f6d9272124712b7c7fa4a931c24135ec09
buang <139893805+buang5516@users.noreply.github.com>
2024-09-07 13:41:00 +0800

    Update BUANGHUB.lua

--- BUANGHUB.lua
+++ BUANGHUB.lua
@@ -5,7 +5,7 @@ end
 task.wait( math.random() )
 
 local games = {
-	[12229756] = 'https://api.luarmor.net/files/v3/loaders/2693a688362ddb52f452a288ea86ba89.lua', -- ALS
+	[12229756] = 'https://api.luarmor.net/files/v3/loaders/223ebf430943b6eeb6b81a550e5dcb52.lua', -- ALS
 	[5292947] =  'https://api.luarmor.net/files/v3/loaders/5626ab81ffceae865d22d54cb5042edf.lua', -- ASTD
 	[15022320] = 'https://api.luarmor.net/files/v3/loaders/5626ab81ffceae865d22d54cb5042edf.lua', -- TTD
 	[5102326] = 'https://api.luarmor.net/files/v3/loaders/1b4c42f5913d7a5b7be56ee7766eb814.lua', -- ACD
@@ -17,11 +17,10 @@ local games = {
 if games[game.CreatorId] then
 	task.wait( math.random() )
 	if game.CreatorId == 34121350 or game.CreatorId == 12229756 or game.CreatorId == 15762744 then
-		if game.CreatorId == 15762744 then print('Loaded') end;
 		repeat
 			loadstring(game:HttpGet(games[game.CreatorId]))()
 			task.wait(10)
-		until getgenv().buanghub ~= nil;
+		until getgenv().buanghub ~= nil or getgenv().buang_key ~= nil;
 	else
 		loadstring(game:HttpGet(games[game.CreatorId]))()
 	end
