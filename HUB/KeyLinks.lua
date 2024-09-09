ffdac4bb8988da705d4a3a08122df588731bc2cc
buang <139893805+buang5516@users.noreply.github.com>
2024-09-09 11:54:42 +0800

    Update KeyLinks.lua

--- HUB/KeyLinks.lua
+++ HUB/KeyLinks.lua
@@ -1,7 +1,7 @@
 local key_links = {
   [33859442] = "https://ads.luarmor.net/get_key?for=Anime_Royale_FREE-LYTFQPGJcPvz", -- Anime Royale
   [12229756] = "https://ads.luarmor.net/get_key?for=Anime_Last_Stand_FREE-YIBgoiFGkCMs", -- Anime Last Stand
-  [17219742] = "" -- Anime Vanguards
+  [17219742] = "https://ads.luarmor.net/get_key?for=Anime_Vanguards_Free-JwgXfEFiVfqv" -- Anime Vanguards
 }
 
 return key_links;
