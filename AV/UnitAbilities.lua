ed4315b96a1bc951eaa2b1bed38efea533ba933e
buang <139893805+buang5516@users.noreply.github.com>
2024-09-23 23:30:26 +0800

    Create UnitAbilities.lua

---
+++ AV/UnitAbilities.lua
@@ -0,0 +1,7 @@
+local abilities = {
+  ["Song Jinwu (Monarch)"] = { Ability = "Arise", MinLevel = 9 },
+  ["Alocard (Vampire King)"] = { Ability = "Restraint: Level 0", MinLevel = 12 },
+  ["Akazo (Destructive)"] = { Ability = "Compass Needle", MinLevel = 9 },
+  ["Blossom"] = { Ability = "Medical Chakra", MinLevel = 4 }
+}
+return abilities;
