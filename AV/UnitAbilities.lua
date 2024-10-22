e8dcb80710fb45ad8924e39e313ad667f1107bb4
buang <139893805+buang5516@users.noreply.github.com>
2024-10-23 04:24:38 +0800

    Update UnitAbilities.lua

--- AV/UnitAbilities.lua
+++ AV/UnitAbilities.lua
@@ -2,6 +2,13 @@ local abilities = {
   ["Song Jinwu (Monarch)"] = { Ability = "Arise", MinLevel = 9 },
   ["Alocard (Vampire King)"] = { Ability = "Restraint: Level 0", MinLevel = 12 },
   ["Akazo (Destructive)"] = { Ability = "Compass Needle", MinLevel = 9 },
-  ["Blossom"] = { Ability = "Medical Chakra", MinLevel = 4 }
+  ["Blossom"] = { Ability = "Medical Chakra", MinLevel = 4 },
+  ["Todu (Unleashed)"] = { Ability = "Boogie", MinLevel = 1 },
+  ["Tuji (Sorcerer Killer)"] = { Ability = "Cursed Inventory", MinLevel = 12 },
+  ["Akazo (Destructive)"] = { Ability = "Compass Needle", MinLevel = 9 },
+  ["Sukono"] = { Ability = "Sinister Sanctum", MinLevel = 9 },
+  ["Todu"] = { Ability = "Boogie", MinLevel = 1 },
+  ["Jag-o (Volcanic)"] = { Ability = "Iron Coffin", MinLevel = 10 },
+  ["Gujo (Infinity)"] = { Ability = "Void", MinLevel = 9 }
 }
 return abilities;
