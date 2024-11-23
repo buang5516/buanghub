e7f458459d934518c804f53f2d81bdbf29201bca
buang <139893805+buang5516@users.noreply.github.com>
2024-11-23 20:45:39 +0800

    Update ModifierCards.lua

--- AV/ModifierCards.lua
+++ AV/ModifierCards.lua
@@ -1,402 +1,400 @@
 local cards = { 
-	["Cards"] = { 
-		["Dungeon"] = { 
-			["Additive"] = { 
-				["Strong"] = { 
-					["Tiers"] = { 
-						[3] = { 
-							["Rarity"] = "Common",
-							["Multiplier"] = 3,
-							["Weight"] = 14,
-							} ,
-						[5] = { 
-							["Rarity"] = "Rare",
-							["Multiplier"] = 5,
-							["Weight"] = 22,
-							} ,
-						[4] = { 
-							["Rarity"] = "Uncommon",
-							["Multiplier"] = 4,
-							["Weight"] = 10,
-							} ,
-						} ,
+	["Dungeon"] = { 
+		["Additive"] = { 
+			["Strong"] = { 
+				["Tiers"] = { 
+					[3] = { 
+						["Rarity"] = "Common",
+						["Multiplier"] = 3,
+						["Weight"] = 14,
 					} ,
-				["Harvest"] = { 
-					["Tiers"] = { 
-						[4] = { 
-							["Rarity"] = "Super Rare",
-							["Multiplier"] = 2,
-							["Weight"] = 40,
-							} ,
-						} ,
+					[5] = { 
+						["Rarity"] = "Rare",
+						["Multiplier"] = 5,
+						["Weight"] = 22,
 					} ,
-				["Planning Ahead"] = { 
-					["Tiers"] = { 
-						[4] = { 
-							["Rarity"] = "Super Rare",
-							["Multiplier"] = 1,
-							["Weight"] = 30,
-							} ,
-						} ,
-					} ,
-				["Precise Attack"] = { 
-					["Tiers"] = { 
-						[4] = { 
-							["Rarity"] = "Super Rare",
-							["Multiplier"] = 1,
-							["Weight"] = 30,
-							} ,
-						} ,
-					} ,
-				["Range"] = { 
-					["Tiers"] = { 
-						[1] = { 
-							["Rarity"] = "Common",
-							["Multiplier"] = 0.1,
-							["Weight"] = 15,
-							} ,
-						[2] = { 
-							["Rarity"] = "Uncommon",
-							["Multiplier"] = 0.2,
-							["Weight"] = 14,
-							} ,
-						} ,
-					} ,
-				["Fast"] = { 
-					["Tiers"] = { 
-						[3] = { 
-							["Rarity"] = "Uncommon",
-							["Multiplier"] = 3,
-							["Weight"] = 10,
-							} ,
-						[2] = { 
-							["Rarity"] = "Common",
-							["Multiplier"] = 2,
-							["Weight"] = 13,
-							} ,
-						} ,
-					} ,
-				["Common Loot"] = { 
-					["Tiers"] = { 
-						[3] = { 
-							["Rarity"] = "Rare",
-							["Multiplier"] = 1,
-							["Weight"] = 19,
-							} ,
-						} ,
-					} ,
-				["Champions"] = { 
-					["Tiers"] = { 
-						[3] = { 
-							["Rarity"] = "Rare",
-							["Multiplier"] = 1,
-							["Weight"] = 22,
-							} ,
-						} ,
-					} ,
-				["Uncommon Loot"] = { 
-					["Tiers"] = { 
-						[3] = { 
-							["Rarity"] = "Rare",
-							["Multiplier"] = 1,
-							["Weight"] = 15,
-							} ,
-						} ,
-					} ,
-				["Dodge"] = { 
-					["Tiers"] = { 
-						[3] = { 
-							["Rarity"] = "Common",
-							["Multiplier"] = 3,
-							["Weight"] = 13,
-							} ,
-						[5] = { 
-							["Rarity"] = "Rare",
-							["Multiplier"] = 5,
-							["Weight"] = 22,
-							} ,
-						[4] = { 
-							["Rarity"] = "Uncommon",
-							["Multiplier"] = 4,
-							["Weight"] = 10,
-							} ,
-						} ,
-					} ,
-				["Press It"] = { 
-					["Tiers"] = { 
-						[2] = { 
-							["Rarity"] = "Uncommon",
-							["Multiplier"] = 0.5,
-							["Weight"] = 14,
-							} ,
-						} ,
-					} ,
-				["Slayer"] = { 
-					["Tiers"] = { 
-						[1] = { 
-							["Rarity"] = "Common",
-							["Multiplier"] = 0.5,
-							["Weight"] = 15,
-							} ,
-						[2] = { 
-							["Rarity"] = "Uncommon",
-							["Multiplier"] = 0.75,
-							["Weight"] = 14,
-							} ,
-						} ,
-					} ,
-				["Cooldown"] = { 
-					["Tiers"] = { 
-						[1] = { 
-							["Rarity"] = "Common",
-							["Multiplier"] = 0.05,
-							["Weight"] = 15,
-							} ,
-						[2] = { 
-							["Rarity"] = "Uncommon",
-							["Multiplier"] = 0.075,
-							["Weight"] = 14,
-							} ,
-						} ,
-					} ,
-				["Damage"] = { 
-					["Tiers"] = { 
-						[1] = { 
-							["Rarity"] = "Common",
-							["Multiplier"] = 0.125,
-							["Weight"] = 15,
-							} ,
-						[2] = { 
-							["Rarity"] = "Uncommon",
-							["Multiplier"] = 0.18,
-							["Weight"] = 14,
-							} ,
-						} ,
-					} ,
-				} ,
-			["Starting"] = { 
-				["No Trait No Problem"] = { 
-					["Tiers"] = { 
-						[1] = { 
-							["Rarity"] = "Common",
-							["Multiplier"] = 1,
-							["Weight"] = 100,
-							} ,
-						} ,
-					} ,
-				["Fisticuffs"] = { 
-					["Tiers"] = { 
-						[1] = { 
-							["Rarity"] = "Common",
-							["Multiplier"] = 1,
-							["Weight"] = 100,
-							} ,
-						} ,
-					} ,
-				["Warding off Evil"] = { 
-					["Tiers"] = { 
-						[1] = { 
-							["Rarity"] = "Common",
-							["Multiplier"] = 1,
-							["Weight"] = 100,
-							} ,
-						} ,
-					} ,
-				["King's Burden"] = { 
-					["Tiers"] = { 
-						[1] = { 
-							["Rarity"] = "Common",
-							["Multiplier"] = 1,
-							["Weight"] = 100,
-							} ,
-						} ,
-					} ,
-				["Money Surge"] = { 
-					["Tiers"] = { 
-						[1] = { 
-							["Rarity"] = "Common",
-							["Multiplier"] = 1,
-							["Weight"] = 100,
-							} ,
-						} ,
+					[4] = { 
+						["Rarity"] = "Uncommon",
+						["Multiplier"] = 4,
+						["Weight"] = 10,
 					} ,
 				} ,
 			} ,
-		["Default"] = { 
-			["Additive"] = { 
-				} ,
-			["Starting"] = { 
-				["Revitalize"] = { 
-					["Tiers"] = { 
-						[1] = { 
-							["Rarity"] = "Common",
-							["Multiplier"] = 1,
-							["Weight"] = 100,
-							} ,
-						} ,
-					} ,
-				["Exploding"] = { 
-					["Tiers"] = { 
-						[1] = { 
-							["Rarity"] = "Common",
-							["Multiplier"] = 1,
-							["Weight"] = 100,
-							} ,
-						} ,
-					} ,
-				["Thrice"] = { 
-					["Tiers"] = { 
-						[1] = { 
-							["Rarity"] = "Common",
-							["Multiplier"] = 1,
-							["Weight"] = 100,
-							} ,
-						} ,
-					} ,
-				["Immunity"] = { 
-					["Tiers"] = { 
-						[1] = { 
-							["Rarity"] = "Common",
-							["Multiplier"] = 1,
-							["Weight"] = 100,
-							} ,
-						} ,
-					} ,
-				["Quake"] = { 
-					["Tiers"] = { 
-						[1] = { 
-							["Rarity"] = "Common",
-							["Multiplier"] = 1,
-							["Weight"] = 100,
-							} ,
-						} ,
-					} ,
-				["Champions"] = { 
-					["Tiers"] = { 
-						[1] = { 
-							["Rarity"] = "Common",
-							["Multiplier"] = 1,
-							["Weight"] = 100,
-							} ,
-						} ,
+			["Harvest"] = { 
+				["Tiers"] = { 
+					[4] = { 
+						["Rarity"] = "Super Rare",
+						["Multiplier"] = 2,
+						["Weight"] = 40,
 					} ,
 				} ,
 			} ,
-		["Paragon"] = { 
-			["Additive"] = { 
-				["Strong"] = { 
-					["Tiers"] = { 
-						[1] = { 
-							["Rarity"] = "Common",
-							["Multiplier"] = 1,
-							["Weight"] = 100,
-							} ,
-						} ,
-					} ,
-				["Dodge"] = { 
-					["Tiers"] = { 
-						[1] = { 
-							["Rarity"] = "Common",
-							["Multiplier"] = 1,
-							["Weight"] = 100,
-							} ,
-						} ,
-					} ,
-				["Drowsy"] = { 
-					["Tiers"] = { 
-						[1] = { 
-							["Rarity"] = "Common",
-							["Multiplier"] = 1,
-							["Weight"] = 100,
-							} ,
-						} ,
-					} ,
-				["Shielded"] = { 
-					["Tiers"] = { 
-						[1] = { 
-							["Rarity"] = "Common",
-							["Multiplier"] = 1,
-							["Weight"] = 100,
-							} ,
-						} ,
-					} ,
-				["Regen"] = { 
-					["Tiers"] = { 
-						[1] = { 
-							["Rarity"] = "Common",
-							["Multiplier"] = 1,
-							["Weight"] = 100,
-							} ,
-						} ,
-					} ,
-				["Fast"] = { 
-					["Tiers"] = { 
-						[1] = { 
-							["Rarity"] = "Common",
-							["Multiplier"] = 1,
-							["Weight"] = 100,
-							} ,
-						} ,
+			["Planning Ahead"] = { 
+				["Tiers"] = { 
+					[4] = { 
+						["Rarity"] = "Super Rare",
+						["Multiplier"] = 1,
+						["Weight"] = 30,
 					} ,
 				} ,
-			["Starting"] = { 
-				["Revitalize"] = { 
-					["Tiers"] = { 
-						[1] = { 
-							["Rarity"] = "Common",
-							["Multiplier"] = 1,
-							["Weight"] = 100,
-							} ,
-						} ,
+			} ,
+			["Precise Attack"] = { 
+				["Tiers"] = { 
+					[4] = { 
+						["Rarity"] = "Super Rare",
+						["Multiplier"] = 1,
+						["Weight"] = 30,
 					} ,
-				["Exploding"] = { 
-					["Tiers"] = { 
-						[1] = { 
-							["Rarity"] = "Common",
-							["Multiplier"] = 1,
-							["Weight"] = 100,
-							} ,
-						} ,
+				} ,
+			} ,
+			["Range"] = { 
+				["Tiers"] = { 
+					[1] = { 
+						["Rarity"] = "Common",
+						["Multiplier"] = 0.1,
+						["Weight"] = 15,
 					} ,
-				["Thrice"] = { 
-					["Tiers"] = { 
-						[1] = { 
-							["Rarity"] = "Common",
-							["Multiplier"] = 1,
-							["Weight"] = 100,
-							} ,
-						} ,
+					[2] = { 
+						["Rarity"] = "Uncommon",
+						["Multiplier"] = 0.2,
+						["Weight"] = 14,
 					} ,
-				["Immunity"] = { 
-					["Tiers"] = { 
-						[1] = { 
-							["Rarity"] = "Common",
-							["Multiplier"] = 1,
-							["Weight"] = 100,
-							} ,
-						} ,
+				} ,
+			} ,
+			["Fast"] = { 
+				["Tiers"] = { 
+					[3] = { 
+						["Rarity"] = "Uncommon",
+						["Multiplier"] = 3,
+						["Weight"] = 10,
 					} ,
-				["Quake"] = { 
-					["Tiers"] = { 
-						[1] = { 
-							["Rarity"] = "Common",
-							["Multiplier"] = 1,
-							["Weight"] = 100,
-							} ,
-						} ,
+					[2] = { 
+						["Rarity"] = "Common",
+						["Multiplier"] = 2,
+						["Weight"] = 13,
 					} ,
-				["Champions"] = { 
-					["Tiers"] = { 
-						[1] = { 
-							["Rarity"] = "Common",
-							["Multiplier"] = 1,
-							["Weight"] = 100,
-							} ,
-						} ,
+				} ,
+			} ,
+			["Common Loot"] = { 
+				["Tiers"] = { 
+					[3] = { 
+						["Rarity"] = "Rare",
+						["Multiplier"] = 1,
+						["Weight"] = 19,
+					} ,
+				} ,
+			} ,
+			["Champions"] = { 
+				["Tiers"] = { 
+					[3] = { 
+						["Rarity"] = "Rare",
+						["Multiplier"] = 1,
+						["Weight"] = 22,
+					} ,
+				} ,
+			} ,
+			["Uncommon Loot"] = { 
+				["Tiers"] = { 
+					[3] = { 
+						["Rarity"] = "Rare",
+						["Multiplier"] = 1,
+						["Weight"] = 15,
+					} ,
+				} ,
+			} ,
+			["Dodge"] = { 
+				["Tiers"] = { 
+					[3] = { 
+						["Rarity"] = "Common",
+						["Multiplier"] = 3,
+						["Weight"] = 13,
+					} ,
+					[5] = { 
+						["Rarity"] = "Rare",
+						["Multiplier"] = 5,
+						["Weight"] = 22,
+					} ,
+					[4] = { 
+						["Rarity"] = "Uncommon",
+						["Multiplier"] = 4,
+						["Weight"] = 10,
+					} ,
+				} ,
+			} ,
+			["Press It"] = { 
+				["Tiers"] = { 
+					[2] = { 
+						["Rarity"] = "Uncommon",
+						["Multiplier"] = 0.5,
+						["Weight"] = 14,
+					} ,
+				} ,
+			} ,
+			["Slayer"] = { 
+				["Tiers"] = { 
+					[1] = { 
+						["Rarity"] = "Common",
+						["Multiplier"] = 0.5,
+						["Weight"] = 15,
+					} ,
+					[2] = { 
+						["Rarity"] = "Uncommon",
+						["Multiplier"] = 0.75,
+						["Weight"] = 14,
+					} ,
+				} ,
+			} ,
+			["Cooldown"] = { 
+				["Tiers"] = { 
+					[1] = { 
+						["Rarity"] = "Common",
+						["Multiplier"] = 0.05,
+						["Weight"] = 15,
+					} ,
+					[2] = { 
+						["Rarity"] = "Uncommon",
+						["Multiplier"] = 0.075,
+						["Weight"] = 14,
+					} ,
+				} ,
+			} ,
+			["Damage"] = { 
+				["Tiers"] = { 
+					[1] = { 
+						["Rarity"] = "Common",
+						["Multiplier"] = 0.125,
+						["Weight"] = 15,
+					} ,
+					[2] = { 
+						["Rarity"] = "Uncommon",
+						["Multiplier"] = 0.18,
+						["Weight"] = 14,
 					} ,
 				} ,
 			} ,
 		} ,
-	}
+		["Starting"] = { 
+			["No Trait No Problem"] = { 
+				["Tiers"] = { 
+					[1] = { 
+						["Rarity"] = "Common",
+						["Multiplier"] = 1,
+						["Weight"] = 100,
+					} ,
+				} ,
+			} ,
+			["Fisticuffs"] = { 
+				["Tiers"] = { 
+					[1] = { 
+						["Rarity"] = "Common",
+						["Multiplier"] = 1,
+						["Weight"] = 100,
+					} ,
+				} ,
+			} ,
+			["Warding off Evil"] = { 
+				["Tiers"] = { 
+					[1] = { 
+						["Rarity"] = "Common",
+						["Multiplier"] = 1,
+						["Weight"] = 100,
+					} ,
+				} ,
+			} ,
+			["King's Burden"] = { 
+				["Tiers"] = { 
+					[1] = { 
+						["Rarity"] = "Common",
+						["Multiplier"] = 1,
+						["Weight"] = 100,
+					} ,
+				} ,
+			} ,
+			["Money Surge"] = { 
+				["Tiers"] = { 
+					[1] = { 
+						["Rarity"] = "Common",
+						["Multiplier"] = 1,
+						["Weight"] = 100,
+					} ,
+				} ,
+			} ,
+		} ,
+	} ,
+	["Default"] = { 
+		["Additive"] = { 
+		} ,
+		["Starting"] = { 
+			["Revitalize"] = { 
+				["Tiers"] = { 
+					[1] = { 
+						["Rarity"] = "Common",
+						["Multiplier"] = 1,
+						["Weight"] = 100,
+					} ,
+				} ,
+			} ,
+			["Exploding"] = { 
+				["Tiers"] = { 
+					[1] = { 
+						["Rarity"] = "Common",
+						["Multiplier"] = 1,
+						["Weight"] = 100,
+					} ,
+				} ,
+			} ,
+			["Thrice"] = { 
+				["Tiers"] = { 
+					[1] = { 
+						["Rarity"] = "Common",
+						["Multiplier"] = 1,
+						["Weight"] = 100,
+					} ,
+				} ,
+			} ,
+			["Immunity"] = { 
+				["Tiers"] = { 
+					[1] = { 
+						["Rarity"] = "Common",
+						["Multiplier"] = 1,
+						["Weight"] = 100,
+					} ,
+				} ,
+			} ,
+			["Quake"] = { 
+				["Tiers"] = { 
+					[1] = { 
+						["Rarity"] = "Common",
+						["Multiplier"] = 1,
+						["Weight"] = 100,
+					} ,
+				} ,
+			} ,
+			["Champions"] = { 
+				["Tiers"] = { 
+					[1] = { 
+						["Rarity"] = "Common",
+						["Multiplier"] = 1,
+						["Weight"] = 100,
+					} ,
+				} ,
+			} ,
+		} ,
+	} ,
+	["Paragon"] = { 
+		["Additive"] = { 
+			["Strong"] = { 
+				["Tiers"] = { 
+					[1] = { 
+						["Rarity"] = "Common",
+						["Multiplier"] = 1,
+						["Weight"] = 100,
+					} ,
+				} ,
+			} ,
+			["Dodge"] = { 
+				["Tiers"] = { 
+					[1] = { 
+						["Rarity"] = "Common",
+						["Multiplier"] = 1,
+						["Weight"] = 100,
+					} ,
+				} ,
+			} ,
+			["Drowsy"] = { 
+				["Tiers"] = { 
+					[1] = { 
+						["Rarity"] = "Common",
+						["Multiplier"] = 1,
+						["Weight"] = 100,
+					} ,
+				} ,
+			} ,
+			["Shielded"] = { 
+				["Tiers"] = { 
+					[1] = { 
+						["Rarity"] = "Common",
+						["Multiplier"] = 1,
+						["Weight"] = 100,
+					} ,
+				} ,
+			} ,
+			["Regen"] = { 
+				["Tiers"] = { 
+					[1] = { 
+						["Rarity"] = "Common",
+						["Multiplier"] = 1,
+						["Weight"] = 100,
+					} ,
+				} ,
+			} ,
+			["Fast"] = { 
+				["Tiers"] = { 
+					[1] = { 
+						["Rarity"] = "Common",
+						["Multiplier"] = 1,
+						["Weight"] = 100,
+					} ,
+				} ,
+			} ,
+		} ,
+		["Starting"] = { 
+			["Revitalize"] = { 
+				["Tiers"] = { 
+					[1] = { 
+						["Rarity"] = "Common",
+						["Multiplier"] = 1,
+						["Weight"] = 100,
+					} ,
+				} ,
+			} ,
+			["Exploding"] = { 
+				["Tiers"] = { 
+					[1] = { 
+						["Rarity"] = "Common",
+						["Multiplier"] = 1,
+						["Weight"] = 100,
+					} ,
+				} ,
+			} ,
+			["Thrice"] = { 
+				["Tiers"] = { 
+					[1] = { 
+						["Rarity"] = "Common",
+						["Multiplier"] = 1,
+						["Weight"] = 100,
+					} ,
+				} ,
+			} ,
+			["Immunity"] = { 
+				["Tiers"] = { 
+					[1] = { 
+						["Rarity"] = "Common",
+						["Multiplier"] = 1,
+						["Weight"] = 100,
+					} ,
+				} ,
+			} ,
+			["Quake"] = { 
+				["Tiers"] = { 
+					[1] = { 
+						["Rarity"] = "Common",
+						["Multiplier"] = 1,
+						["Weight"] = 100,
+					} ,
+				} ,
+			} ,
+			["Champions"] = { 
+				["Tiers"] = { 
+					[1] = { 
+						["Rarity"] = "Common",
+						["Multiplier"] = 1,
+						["Weight"] = 100,
+					} ,
+				} ,
+			} ,
+		} ,
+	} ,
+}
 
 return cards;
