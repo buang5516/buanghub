6fb7bb288e356b709fa398b00b478c649d3b88dd
buang <139893805+buang5516@users.noreply.github.com>
2024-12-17 03:53:29 +0800

    Update CurrencyIcons.lua

--- ALS/CurrencyIcons.lua
+++ ALS/CurrencyIcons.lua
@@ -1,13 +1,13 @@
-	local icons = {
-		Emeralds = '<:emerald:1204766658397343805>',
-		Gold = '<:gold:1217525743408648253>',
-		Rerolls = '<:rerolls:1216376860804382860>',
-		RaidTokens = '<:rt:1225071926427254936>',
-		Shells = '<:shells:1256934942475026455>',
-		Jewels = '<:als_jewels:1265957290251522089>',
-		Candy = '<:alscandy:1300366683826814996>',
-    BossRushTokens = '<:BossRushTokens:1318304496404664431>',
-    SkinTicket = '<:SkinTicket:1318304829172617306>'
-	}
+local icons = {
+	Emeralds = '<:emerald:1204766658397343805>',
+	Gold = '<:gold:1217525743408648253>',
+	Rerolls = '<:rerolls:1216376860804382860>',
+	RaidTokens = '<:rt:1225071926427254936>',
+	Shells = '<:shells:1256934942475026455>',
+	Jewels = '<:als_jewels:1265957290251522089>',
+	Candy = '<:alscandy:1300366683826814996>',
+    	BossRushTokens = '<:BossRushTokens:1318304496404664431>',
+   	 SkinTicket = '<:SkinTicket:1318304829172617306>'
+}
 
 return icons;
