d58fdbb40191e62abbc3e033ba91c3d0cb3e9fab
buang <139893805+buang5516@users.noreply.github.com>
2024-12-26 03:28:24 +0800

    Update KeyLinks.lua

--- HUB/KeyLinks.lua
+++ HUB/KeyLinks.lua
@@ -1,7 +1,8 @@
 local key_links = {
   [33859442] = "https://ads.luarmor.net/get_key?for=Anime_Royale_FREE-LYTFQPGJcPvz", -- Anime Royale
   [12229756] = "https://ads.luarmor.net/get_key?for=Anime_Last_Stand_FREE-YIBgoiFGkCMs", -- Anime Last Stand
-  [17219742] = "https://ads.luarmor.net/get_key?for=Anime_Vanguards_Free-JwgXfEFiVfqv" -- Anime Vanguards
+  [17219742] = "https://ads.luarmor.net/get_key?for=Anime_Vanguards_Free-JwgXfEFiVfqv", -- Anime Vanguards
+  [10611639] = "https://ads.luarmor.net/get_key?for=Anime_Adventures_BUANG_HUB-rKFDKSeZFbIm" -- Anime Adventures
 }
 
 return key_links;
