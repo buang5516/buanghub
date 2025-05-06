20e82de7df8a108e733eb340cb6d5c31a8ffe156
buang <139893805+buang5516@users.noreply.github.com>
2025-05-06 23:20:45 +0800

    Update codes.lua

--- codes.lua
+++ codes.lua
@@ -1,33 +1,8 @@
 local codes = {
-    "AnniEventWEEK!!",
-    "DAILYRewards?!",
-    "AnniDungeonRelease!",
-    "Happy1stAnni!!",
-    "AnniUpdSoon?!",
-    "AnimeLastDelay",
-    "OtakuNoob2Pro!",
-    "ShadowMonarch!",
-    "Dungeons!",
-    "AnotherSecretCode!",
-    "SecretCode!",
-    "NewUi!",
-    "BattlepassReset!",
-    "GreatWarWow!",
-    "NewCaverns!",
-    "NewGlobalBoss!",
-    "SORRYforIssues!!",
-    "NewGodly!",
-    "ChristmasRaid!",
-    "EventBP!",
-    "Sub2OtakuGaming",
-    "BossRush!",
-    "SkinBanner!",
-    "LateUpdSorry!",
-    "BossIGCool!",
-    "NewUpdateS00N",
-    "Drops!",
-    "Evos!",
-    "Tournaments!"
+    "SuperSecretCODE",
+    "KingOfHeroes!",
+    "NewBattlepass!",
+    "GodlyUpdate!"
 }
 
 return codes;
