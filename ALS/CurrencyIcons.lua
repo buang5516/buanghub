1e7889c03e5b741a09eb7d56dd1c78c00f806db1
buang <139893805+buang5516@users.noreply.github.com>
2025-06-30 17:07:37 +0800

    Update CurrencyIcons.lua

--- ALS/CurrencyIcons.lua
+++ ALS/CurrencyIcons.lua
@@ -1,5 +1,5 @@
 local icons = {
-	Emeralds = '<:emerald:1204766658397343805>',
+	Emeralds = "<:emerald:1389165843966984192>", --'<:emerald:1204766658397343805>',
 	Gold = '<:gold:1217525743408648253>',
 	Rerolls = '<:rerolls:1216376860804382860>',
 	RaidTokens = '<:rt:1225071926427254936>',
@@ -11,7 +11,12 @@ local icons = {
 	Anniversary1Tokens = '<:Anniversary1Tokens:1338944371701317864>',
 	Anniversary1Tickets = '<:Anniversary1Tickets:1338944972015145070>',
 	EasterEgg = '<:easteregg:1366617917076144221>',
-	LargeEasterEgg = '<:largeEasterEgg:1366618509437566997>'
+	LargeEasterEgg = '<:largeEasterEgg:1366618509437566997>',
+	SummerCorals = "<:Corals:1389166758102958101>",
+	TitanRushTokens = "<:TitanToken:1389168855779381328>",
+	GrailRushTokens = "<:GrailToken:1389169246952755271>",
+	DivineTokens = "<:DivineToken:1389169805407424552>",
+	InfernalTokens = "<:InfernalToken:1389169992758726767>"
 }
 
 return icons;
